64fd9a9eeafef6d9856ae95bd249b76686460390
Baraahbsharat <baraaberor@gmail.com>
2020-12-23 16:35:29 +0200

    your message

---
+++ About-us.js
@@ -0,0 +1,12 @@
+ var name = prompt("Enter your name: ");
+alert("Your name is " + name);
+    var age = prompt("Enter your age:");
+alert("Your age is " + age);
+
+if(age > 16){
+    alert('You can buy');
+}
+else{
+    alert("You are not allow to buy");
+}
+    
