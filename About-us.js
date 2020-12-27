83bb4111b2fdedc5c52254bbd6b8163fe0780359
Baraahbsharat <baraaberor@gmail.com>
2020-12-27 15:37:17 +0200

    add

--- About-us.js
+++ About-us.js
@@ -2,11 +2,12 @@
 alert("Your name is " + name);
     var age = prompt("Enter your age:");
 
-
-
-while (age === '') {
-    age = prompt(' Your age is null, Enter your age');  
-  }
+    userage (age);
+    function userage (uage){
+        while (uage === '') {
+            uage = prompt(' Your age is null, Enter your age');  
+          }}
+    
   alert("Your age is " + age);
   if(age > 16){
     alert('You can buy');
@@ -15,7 +16,9 @@ else{
     alert("You are not allow to buy");
 }
 var order = prompt("Each box of chocolate had 24 piece, How many box do you want ?")  
-for ( var i = 0; i<order; i = i + 1 ) {
-    document.write( '<img src="box.jpg">' )
-}
 
+photosnumber ();
+function photosnumber () {
+    for ( var i = 0; i<order; i = i + 1 ) {
+        document.write( '<img src="box.jpg">' )
+}}
